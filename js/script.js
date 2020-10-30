4ba551e3fed3ceb0b1b9c2b5a0deb7f3b4497a24
Anna Bychkova <bychkova.dev@gmail.com>
2020-10-30 21:12:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,44 +28,50 @@
  */
 // const secs = Math.floor((time % (1000 * 60)) / 1000);
 
-
-
 const refs = {
   fieldDays: document.querySelector('.field>span[data-value="days"]'),
-  fieldHours:document.querySelector('.field>span[data-value="hours"]'),
+  fieldHours: document.querySelector('.field>span[data-value="hours"]'),
   fieldMinutes: document.querySelector('.field>span[data-value="mins"]'),
   fieldSeconds: document.querySelector('.field>span[data-value="secs"]'),
-}
+};
 
-class CountdownTimer{ 
-  constructor({selector,targetDate}) {
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
-}
-start(){    
+  }
+  start() {
+    this.renderTime();
     setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = (currentTime - this.targetDate)*-1;
-      const {days,hours,mins,secs} = this.getTimeComponents(deltaTime);
-      console.log(`${days}:${hours}:${mins}:${secs}`)
-      this.updateClockFace({days,hours,mins,secs});
-    }, 1000)
-  };
-getTimeComponents(time) {
- const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
-  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-  return {days,hours,mins,secs}};
-pad(value) {
-  return String(value).padStart(2,'0')};
-updateClockFace({ days, hours, mins, secs }) {
-  refs.fieldDays.textContent = `${days}`;
-  refs.fieldHours.textContent = `${hours}`;
-  refs.fieldMinutes.textContent = `${mins}`;
-  refs.fieldSeconds.textContent = `${secs}`;
+      this.renderTime();
+    }, 1000);
+  }
+  renderTime() {
+    const currentTime = Date.now();
+    const deltaTime = (currentTime - this.targetDate) * -1;
+    const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
+    console.log(`${days}:${hours}:${mins}:${secs}`);
+    this.updateClockFace({ days, hours, mins, secs });
+  }
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  updateClockFace({ days, hours, mins, secs }) {
+    refs.fieldDays.textContent = `${days}`;
+    refs.fieldHours.textContent = `${hours}`;
+    refs.fieldMinutes.textContent = `${mins}`;
+    refs.fieldSeconds.textContent = `${secs}`;
+  }
 }
-};
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
